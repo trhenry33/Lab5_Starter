92cb1747b9ef4ea1d5723a8e045f438f2eee3699
Travis Henry <travman.henry1@gmail.com>
2025-05-06 00:37:47 -0700

    worked on explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,7 +4,53 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  
+  const talkButton = document.querySelector("button");
+  const textToSpeak = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const faceImage = document.querySelector("img");
+
+  let voices = [];
+  function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+ 
+
+    voices.forEach((voice, index) =>{
+      const option = document.createElement("option");
+      option.value = index;
+      option.textContent = voice.name;
+      voiceSelect.appendChild(option);
+    });
+  }
+  //got help on this one
+  if (speechSynthesis.onvoiceschanged!== undefined) {
+    speechSynthesis.onvoiceschanged =populateVoiceList;
+  } else {
+    populateVoiceList();
+  }
+
+
+  function pressToTalk() {
+    const text = textToSpeak.value;
+    const selectedVoice = voices[voiceSelect.value];
+
+    if (text !== '') {
+      const speech = new SpeechSynthesisUtterance(text);
+      speech.voice = selectedVoice;
+
+      speech.onstart =() => {
+        faceImage.src = "assets/images/smiling-open.png"; 
+      };
+
+      speech.onend = ()=> {
+        faceImage.src = "assets/images/smiling.png"; 
+      };
+
+      // Speak the text
+      speechSynthesis.speak(speech);
+    }
+  }
+
+  talkButton.addEventListener("click",pressToTalk);
 
 
 
